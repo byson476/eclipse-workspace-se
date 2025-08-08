690ee8154c0966f908a2730560003609331512c3
byson <byson476@gmail.com>
2025-08-08 10:40:31 +0900

    accoount

---
+++ 99.PRACTICE/src/com/itwill04/array/account/AccountArrayMain.java
@@ -0,0 +1,170 @@
+package com.itwill04.array.account;
+
+public class AccountArrayMain {
+
+	public static void main(String[] args) {
+		System.out.println("-----------배열객체생성 블록{} 초기화-----------");
+		Account acc1=new Account(1111, "KIM", 56000, 0.9);
+		Account[] accounts= {
+				acc1,
+				new Account(2222, "LEE", 58900, 0.5),
+				new Account(3333, "PARK", 78000, 0.4),
+				new Account(4444, "CHOI", 34000, 0.3),
+				new Account(5555, "SIM", 66000, 0.7),
+				new Account(6666, "HONG", 99000, 1.4),
+				new Account(7777, "KIM", 87600, 0.6),
+				new Account(8888, "SOO", 12300, 2.3),
+				new Account(9999, "MIN", 41200, 4.1)
+		};
+		
+		System.out.println("1.은행총계좌수:" + accounts.length);
+		System.out.println("2.은행계좌 전체출력");
+		Account.headerPrint();
+		for(Account tempAccount:accounts) {
+			tempAccount.print();
+		}
+		System.out.println("3.은행계좌 총잔고");
+		int totBalance=0;
+		for(Account tempAccount:accounts) {
+			totBalance+=tempAccount.getBalance();
+		}
+		System.out.println(">> "+totBalance);
+		System.out.println("4.은행 계좌들중에서 계좌번호 3333번계좌 한개 찾아서 출력(계좌번호는 중복되지않는다)");
+		for(int i=0;i<accounts.length;i++) {
+			if(accounts[i].getNo()==3333) {
+				accounts[i].print();
+				break;
+			}
+		}
+		System.out.println("5.은행 계좌들중에서 계좌잔고 50000원이상인 VIP계좌 여러개 찾아서 출력");
+		for (Account account : accounts) {
+			if(account.getBalance()>=50000) {
+				account.print();
+			}
+		}
+		System.out.println("6.은행 계좌들중에서 계좌이율 5.0 이상인 계좌 여러개 찾아서 출력");
+		for (Account account : accounts) {
+			if(account.getIyul()>=5.0) {
+				account.print();
+			}
+		}
+		System.out.println("6.은행 계좌들중에서 이름이 KIM인 계좌 여러개 찾아서 출력[String객체배우고난후에]");
+		for (Account account : accounts) {
+			String tempOwner=account.getOwner();
+			String searchOwner="KIM";
+			/*
+			if(tempOwner==searchOwner) {
+				account.print();
+			}
+			*/
+			if(tempOwner.equals(searchOwner)) {
+				account.print();
+			}
+		}
+		
+		
+		System.out.println("7.6666번계좌 3000원입금");
+		for (int i=0;i<accounts.length;i++) {
+			Account account=accounts[i];
+			if(account.getNo()==6666) {
+				System.out.println(">>입금전");
+				account.print();
+				account.deposit(3000);
+				System.out.println(">>입금후");
+				account.print();
+				break;
+			}
+		}
+		
+		System.out.println("8.9999번계좌 3000원출금[Quiz]");
+		
+		System.out.println("9.계좌잔고순으로 오름[내림]차순정렬");
+		System.out.println("############정렬전##############");
+		for (Account account : accounts) {
+			account.print();
+		}
+		for(int i=0;i<accounts.length-1;i++) {
+			for(int j=0;j<accounts.length-1;j++) {
+				if(accounts[j].getBalance() > accounts[j+1].getBalance()) {
+					//주소값교환
+					Account tempJAccount=accounts[j];
+					accounts[j]=accounts[j+1];
+					accounts[j+1]=tempJAccount;
+				}
+			}
+		}
+		System.out.println("############정렬후##############");
+		for (Account account : accounts) {
+			account.print();
+		}
+		System.out.println("9.계좌번호순으로 오름(내림)차순정렬[Quiz]");
+		for (int i = 0; i < accounts.length-1; i++) {
+			for (int j = 0; j < accounts.length-1; j++) {
+				if(accounts[j].getNo() > accounts[j+1].getNo()) {
+					Account tempAccout=accounts[j];
+					accounts[j]=accounts[j+1];
+					accounts[j+1]=tempAccout;
+				}
+			}
+		}
+		for (Account account : accounts) {
+			account.print();
+		}
+		System.out.println("10.5555계좌한개 이율을 9.9으로변경");
+		for (Account account : accounts) {
+			if(account.getNo()==5555) {
+				System.out.println(">>변경전");
+				account.print();
+				account.setIyul(0.9);
+				System.out.println(">>변경후");
+				account.print();
+				break;
+			}
+		}
+		System.out.println("11.VIP계좌(잔고50000원이상)여러개 잔고 50원 증가");
+		for (Account account : accounts) {
+			if(account.getBalance()>=50000) {
+				account.deposit(50);
+			}
+		}
+		for (Account account : accounts) {
+			account.print();
+		}
+		
+		/********************OPTION***********************
+		  1. accounts배열 객체에 새로생성한계좌객체 추가 
+		  		A. accounts.length+1 개짜리 임시배열생성
+		 		B. accounts의 모든계좌객체 임시배열로 이동
+				C. 임시배열에 새로운  Account객체추가
+		 		D. accounts에 임시배열주소대입
+		 
+		  2. accounts배열 객체에서 계좌번호 4444번계좌객체삭제
+		  	 	A. accounts 에서 4444계좌찾아서 null대입
+			 	B. accounts.length-1 개짜리 임시배열생성
+			 	C. accounts null이아닌 모든계좌 임시배열로이동
+			  	D. accounts 에 임시배열대입 
+		 */
+		System.out.println("----------------------------Account객체추가----------------------------");
+		Account[] tempAccounts=new Account[accounts.length+1];
+		for(int i=0;i<accounts.length;i++) {
+			tempAccounts[i]=accounts[i];
+		}
+		Account newAccount=new Account(1112,"NEW", 45000, 3.5);
+		tempAccounts[tempAccounts.length-1] = newAccount;
+		accounts=tempAccounts;
+		
+		
+		System.out.println("-------추가후출력----------");
+		for(int i=0;i<accounts.length;i++) {
+			accounts[i].print();
+		}
+		System.out.println("----------------------------Account객체삭제[4444]----------------------");
+		
+		System.out.println("-------삭제후출력----------");
+		for(int i=0;i<accounts.length;i++) {
+			accounts[i].print();
+		}
+		
+	}
+}
+
--- 99.PRACTICE/src/com/itwill04/array/car/CarArrayMain.java
+++ 99.PRACTICE/src/com/itwill04/array/car/CarArrayMain.java
@@ -3,48 +3,94 @@ package com.itwill04.array.car;
 public class CarArrayMain {
 
 	public static void main(String[] args) {
-		
-		Car[] carArray= {
-			null,null,new Car("5654", 8),null,null,
-			new Car("3422", 12),null,null,null,null,
-			null,null,null,null,new Car("7789", 11),
-			null,new Car("1120", 10),null,null,null,
-			null,new Car("2389", 9),null,null,null,
-			null,null,null,null,new Car("5555", 6)
-		};
+
+		Car[] carArray = { null, null, new Car("5654", 8), null, null, new Car("3422", 12), null, null, null, null,
+				null, null, null, null, new Car("7789", 11), null, new Car("1120", 10), null, null, null, null,
+				new Car("2389", 9), null, null, null, null, null, null, null, new Car("5555", 6) };
 		/*
-		 null.메쏘드() --> NullPointerException 
+		 * null.메쏘드() --> NullPointerException
 		 */
 		System.out.println("1.전체차량출력");
 		Car.headerPrint();
 		for (int i = 0; i < carArray.length; i++) {
-			if(carArray[i]!=null) {
+			if (carArray[i] != null) {
 				carArray[i].print();
 			}
 		}
+		System.out.println();
 		System.out.println("2.전체주차구획수");
-		System.out.println(">> ");
+		System.out.println(">> " + carArray.length);
+		System.out.println();
 		System.out.println("3.주차가능주차구획수");
-		int parkingCount=0;
-		System.out.println(">>"+parkingCount);
+		int parkingCount = 0;
+		for (Car car : carArray) {
+			if (car != null)
+				parkingCount++;
+		}
+
+		System.out.println(">>" + (carArray.length - parkingCount));
+		System.out.println();
 		System.out.println("4.입차(차량번호 7777번 14시 입차)");
 		/*
-		 * 1. 차량객체생성
-		 * 2. 빈자리찾아서 대입
+		 * 1. 차량객체생성 2. 빈자리찾아서 대입
 		 */
+		Car tempCar = new Car("7777", 14);
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] == null) {
+				carArray[i] = tempCar;
+				break;
+			}
+		}
+		
+		System.out.println();
 		System.out.println("5.차량번호 7789번  차한대 정보출력");
-		System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
-		System.out.println("7.2389번차량 18시 출차");
-		/*
-		 1.2389번 차량 찾기
-		 2.출차(나가는시간대입,요금계산)
-		 3.영수증출력
-		 4.2389번차량 주차장에서 삭제
-		 */
-		System.out.println("99. 2389번차량 출차후전체 차량출력");
 		Car.headerPrint();
-		for(int i=0;i<carArray.length;i++) {
-			if(carArray[i]!=null) {
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo().equals("7789")) {
+				carArray[i].print();
+				break;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getInTime()>=10) {
+				carArray[i].print();
+			}
+		}
+		System.out.println();
+		System.out.println("7.2389번차량 18시 출차");
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo().equals("2389")) {
+				carArray[i].setOutTime(18);
+				carArray[i].calculateFee();
+				break;
+			}
+		}
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo().equals("2389")) {
+				carArray[i].print();
+				break;
+			}
+		}
+		
+		/*
+		 * 1.2389번 차량 찾기 2.출차(나가는시간대입,요금계산) 3.영수증출력 4.2389번차량 주차장에서 삭제
+		 */
+		System.out.println();
+		System.out.println("99. 2389번차량 출차후전체 차량출력");
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo().equals("2389")) {
+				carArray[i]=null;
+				break;
+			}
+		}
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null) {
 				carArray[i].print();
 			}
 		}
