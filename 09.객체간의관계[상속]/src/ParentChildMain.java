6b5871728aecf949c51d10fd269716e058de3f82
byson <byson476@gmail.com>
2025-08-08 12:07:26 +0900

    q

---
+++ 09.객체간의관계[상속]/src/ParentChildMain.java
@@ -0,0 +1,52 @@
+
+class Parent {
+	public int member1;
+	public int member2;
+	public void method1() {
+		System.out.println("Parent.method1()");
+	}
+	public void method2() {
+		System.out.println("Parent.method2()");
+	}
+}
+
+class Child extends Parent{
+/*
+	public int member1;
+	public int member2;
+	public void method1() {
+		System.out.println("Parent.method1()");
+	}
+	public void method2() {
+		System.out.println("Parent.method2()");
+	}
+ */
+	public int member3;
+	public void methos3() {
+		System.out.println("Child.methos3()");
+	}
+	
+}
+public class ParentChildMain {
+	public static void main(String[] args) {
+		System.out.println("-----------child객체---------------");
+		Child c1 = new Child();
+		c1.member1 = 1;
+		c1.member2 = 2;
+		c1.member3 = 3;
+		c1.method1();
+		c1.method2();
+		c1.methos3();
+		System.out.println("c1=" + c1);
+		/*
+		System.out.println("-----------Parent객체---------------");
+		Parent p1 = new Parent();
+		p1.member1 = 1;
+		p1.member2 = 2;
+		p1.method1();
+		p1.method2();
+		System.out.println("p1=" + p1);
+		*/
+		
+	}
+}
